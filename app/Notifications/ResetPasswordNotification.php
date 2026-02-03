22a307a24a643e6b7d3ebd54980c1890062e7573
tzuwiii <142727357+tzucun@users.noreply.github.com>
2026-02-03 12:12:51 +0700

    Several changes

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -30,11 +30,7 @@ class ResetPasswordNotification extends Notification
         ], false));
 
         return (new MailMessage)
-<<<<<<< HEAD
             ->subject('Permintaan Reset Password - SINVENTA')
-=======
-            ->subject('Reset Password - Inventaris Telkom')
->>>>>>> f68f6cdbc853c53cc75033861251d15a729cc2f5
             ->greeting('Halo,')
             ->line('Anda menerima email ini karena ada permintaan reset password akun Inventaris Telkom Anda.')
             ->action('Reset Password Sekarang', $url)
@@ -42,11 +38,7 @@ class ResetPasswordNotification extends Notification
             ->line('Jika tombol "Reset Password Sekarang" tidak dapat diklik, silakan salin dan tempel link berikut ke browser Anda:')
             ->line($url)
             ->line('Jika Anda tidak merasa melakukan permintaan ini, silakan abaikan email ini.')
-<<<<<<< HEAD
             ->salutation('Hormat kami, Tim SINVENTA')
-=======
-            ->salutation('Hormat kami, Tim Inventaris Telkom')
->>>>>>> f68f6cdbc853c53cc75033861251d15a729cc2f5
             
             ->withSymfonyMessage(function ($message) {
             $message->getHeaders()->remove('X-Mailer');
